df41904461fe774870b6dd596f6dba3b59cb88f3
aepii <simentalma@yahoo.com>
2024-04-14 18:44:48 -0500

    Started Inventory

---
+++ src/replicated/Data/Inventory.lua
@@ -0,0 +1,12 @@
+local inventory = {
+	[1] = {
+		Name = "Roblox Baseball Cap";
+        Type = "Accessory";
+        AssetID = 607702162;
+        Value = 5;
+        Rarity = "Basic";
+        Cases = "Basic"
+	};
+}
+	
+return inventory
