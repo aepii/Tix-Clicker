339df4f4da3fe259609fb78646cef63f2d8520df
aepii <simentalma@yahoo.com>
2024-04-14 11:32:29 -0500

    Added Upgrades and ValueUpgrades

---
+++ src/replicated/Data/Upgrades.lua
@@ -0,0 +1,11 @@
+local upgrades = {
+	["Upgrade1"] = {
+		Title = "Tix";
+		Cost = 0;
+		Reward = 1;
+		Object = nil;
+		Image = nil
+	};
+}
+	
+return upgrades
---
+++ src/replicated/Data/ValueUpgrades.lua
@@ -0,0 +1,11 @@
+local valueUpgrades = {
+	["SecUpgrade1"] = {
+		Title = "Tix Piggy Bank";
+		Cost = 2;
+		Modifier = 1.04;
+		Reward = 1;
+		Image = nil
+	};
+}
+	
+return valueUpgrades
